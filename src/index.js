3a3ae9abbb8833f6df0b7ca50e44fd4085db2912
lokosuns <lokosuns@gmail.com>
2022-05-10 21:08:46 +0300

    After 41 lesson samurai's way

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
-import {state} from "./redux/state";
+import store from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-rerenderEntireTree(state);
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,51 +1,73 @@
-import {rerenderEntireTree} from "../render";
+import {profileReduser} from "./profile-reduser";
+import {dialogsReduser} from "./dialogs-reduser";
+import {sidebarReduser} from "./sidebar-reduser";
 
-export const state = {
-    profilePage: {
-        postData: [
-            {id: 1, message: 'Hi!', likesCount: 5},
-            {id: 2, message: 'It\'s my first post', likesCount: 6},
-        ],
-        newPostText: 'Lokomotiv',
+let store = {
+    _state: {
+        profilePage: {
+            postData: [
+                {id: 1, message: 'Hi!', likesCount: 5},
+                {id: 2, message: 'It\'s my first post', likesCount: 6},
+            ],
+            newPostText: 'Lokomotiv',
+        },
+        messagePage: {
+            dialogData: [
+                {id: 1, name: 'Alex'},
+                {id: 2, name: 'Max'},
+                {id: 3, name: 'Lola'},
+                {id: 4, name: 'KirReal'},
+            ],
+            messageData: [
+                {id: 1, message: 'Hi!'},
+                {id: 2, message: 'How is your name?'},
+                {id: 3, message: 'Yo!'},
+                {id: 4, message: 'Yo!'},
+                {id: 5, message: 'Yo!'},
+            ],
+            newMessageText: '',
+        },
+        sidebarPage: {
+            friendsData: [
+                {
+                    id: 1,
+                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxLkbtTa0kfmKizxJgqECQLdlt_xq1R2jEQQ&usqp=CAU',
+                    name: 'Alex'
+                },
+                {
+                    id: 2,
+                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxLkbtTa0kfmKizxJgqECQLdlt_xq1R2jEQQ&usqp=CAU',
+                    name: 'Max'
+                },
+                {
+                    id: 3,
+                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxLkbtTa0kfmKizxJgqECQLdlt_xq1R2jEQQ&usqp=CAU',
+                    name: 'Lola'
+                },
+            ],
+        }
     },
-    messagePage: {
-        dialogData: [
-            {id: 1, name: 'Alex'},
-            {id: 2, name: 'Max'},
-            {id: 3, name: 'Lola'},
-            {id: 4, name: 'KirReal'},
-        ],
-        messageData: [
-            {id: 1, message: 'Hi!'},
-            {id: 2, message: 'How is your name?'},
-            {id: 3, message: 'Yo!'},
-            {id: 4, message: 'Yo!'},
-            {id: 5, message: 'Yo!'},
-        ],
+    _callSubscriber() {
+        console.log('Change')
     },
-    sidebarPage: {
-        friendsData: [
-            {id: 1, avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxLkbtTa0kfmKizxJgqECQLdlt_xq1R2jEQQ&usqp=CAU', name: 'Alex'},
-            {id: 2, avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxLkbtTa0kfmKizxJgqECQLdlt_xq1R2jEQQ&usqp=CAU', name: 'Max'},
-            {id: 3, avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxLkbtTa0kfmKizxJgqECQLdlt_xq1R2jEQQ&usqp=CAU', name: 'Lola'},
-        ],
-    }
+
+    getState() {
+        return this._state
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+
+    dispatch(action) {
+
+        this._state.profilePage = profileReduser(this._state.profilePage, action);
+        this._state.messagePage = dialogsReduser(this._state.messagePage, action);
+        this._state.sidebarPage = sidebarReduser(this._state.sidebarPage, action);
+        this._callSubscriber(this._state);
+
+    },
+
 }
 
-window.state = state;
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-    };
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
+export default store
+window.store = store;
