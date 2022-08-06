cb4db579cc0cfdb30c08f5d8c5b36c86e4d75f43
lokosuns <lokosuns@gmail.com>
2022-08-06 18:46:00 +0300

    Import axios

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -30,7 +30,7 @@ const usersReducer = (state = initialState, action) => {
 
         case SET_USERS:
             return {
-                ...state, users: [...action.users]
+                ...state, users: action.users
             }
 
         default:
