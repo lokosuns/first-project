525388080598c74e2e66a744eee2ac4740c9785e
lokosuns <lokosuns@gmail.com>
2022-08-20 20:28:31 +0300

    Refactoring Thunk

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import {Profile} from "./Profile";
-import axios from "axios";
 import {connect} from "react-redux";
-import {setUserProfile} from "../../redux/profile-reducer";
+import {getUserProfile} from "../../redux/profile-reducer";
 import {useLocation, useNavigate, useParams} from "react-router-dom";
 
- // Декоратор для замены HOC withRouter, т.к. в react-route-dom 6 его уже нет.
+// Декоратор для замены HOC withRouter, т.к. в react-route-dom 6 его уже нет.
  // В дальнейшем при замене классовой компоненты на функциональную, нужно будет использовать только хуки.
 function withRouter(Component) {
     function ComponentWithRouterProps(props) {
@@ -27,9 +26,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.router.params.userId;
         if (!userId) {userId = ''}
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => {
-            this.props.setUserProfile(response.data);
-        })
+        this.props.getUserProfile(userId)
     }
 
 
@@ -46,4 +43,4 @@ let mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps,{setUserProfile})(withRouter(ProfileContainer));
+export default connect(mapStateToProps,{getUserProfile})(withRouter(ProfileContainer));
